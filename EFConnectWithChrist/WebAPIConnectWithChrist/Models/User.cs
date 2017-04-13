2d5f08f26b10565480f47db938abc76c8a96ba50
Stephen Kirkland <sk01417@uga.edu>
2017-04-13 10:18:56 -0400

    Set up my basic Models and Controllers

---
+++ EFConnectWithChrist/WebAPIConnectWithChrist/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIConnectWithChrist.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int MainBusinessID { get; set; }
+        public string MainBusiness { get; set; }
+        public System.DateTime dayAndTimeJoined { get; set; }
+        public int UserTypeID { get; set; }
+        public string UserType { get; set; }
+    }
+}
